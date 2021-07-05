4c10ac6e81d5aff70787df6e4b3289f2fcba2302
luoxin <1813650185@qq.com>
2021-07-05 13:08:02 +0800

    属性管理页面静态搭建

---
+++ src/components/CategorySelector/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="cascade-wraper">
+    <span class="cascade-title">一级分类</span>
+    <!-- 
+        :options="options"
+        @change="handleChange" 
+      -->
+    <el-cascader v-model="category1Id"></el-cascader>
+    <span class="cascade-title">二级分类</span>
+    <el-cascader v-model="category2Id"></el-cascader>
+    <span class="cascade-title">三级分类</span>
+    <el-cascader v-model="category3Id"></el-cascader>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategorySelector',
+  data() {
+    return {
+      category1Id: '',
+      category2Id: '',
+      category3Id: '',
+      category1List: [],
+      category2List: [],
+      category3List: [],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.cascade-wraper {
+  margin-bottom: 20px;
+}
+
+.cascade-title {
+  display: inline-block;
+  font-size: 14px;
+  color: #606266;
+  line-height: 40px;
+  padding: 0 12px 0 0;
+  font-weight: 700;
+}
+
+.el-cascader {
+  margin-right: 10px;
+}
+</style>
--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,13 +1,44 @@
 <template>
-  <div>
-    Attr
+  <div class="attr-wraper">
+    <!-- 三级联动属性分类 -->
+    <el-card shadow="always">
+      <CategorySelector />
+    </el-card>
+
+    <!-- 属性列表 -->
+    <el-card shadow="always" style="margin-top:20px">
+      <el-button type="primary" icon="el-icon-plus">添加属性</el-button>
+      <el-table border :data="attrList" style="width: 100%; margin-top:20px">
+        <el-table-column type="index" label="序号" width="80" align="center"></el-table-column>
+        <el-table-column prop="date" label="属性名称"></el-table-column>
+        <el-table-column prop="name" label="属性值列表"></el-table-column>
+        <el-table-column prop="address" label="操作" width="180"></el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
+import CategorySelector from '@/components/CategorySelector';
+
 export default {
   name: 'Attr',
+  components: {
+    CategorySelector,
+  },
+  data() {
+    return {
+      category1Id: '',
+      category2Id: '',
+      category3Id: '',
+      attrList: [],
+    };
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.attr-wraper {
+  padding: 20px 0;
+}
+</style>
