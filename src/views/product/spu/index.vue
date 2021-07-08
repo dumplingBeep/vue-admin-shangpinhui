34c75fb134afeaed31fa7af85d716f24e585f3ca
luoxin <1813650185@qq.com>
2021-07-08 18:04:17 +0800

    SPU列表展示

--- src/views/product/spu/index.vue
+++ src/views/product/spu/index.vue
@@ -1,13 +1,117 @@
 <template>
-  <div>
-    SPU
+  <div class="spu-wraper">
+    <!-- 分类选择器 -->
+    <el-card>
+      <CategorySelector
+        :category1Id.sync="category1Id"
+        :category2Id.sync="category2Id"
+        :category3Id.sync="category3Id"
+        :isShowList="!isShowSpuList"
+      />
+    </el-card>
+
+    <el-card style="margin-top: 20px">
+      <el-button type="primary" icon="el-icon-plus">添加SPU</el-button>
+
+      <!-- 数据表格 -->
+      <el-table v-loading="loading" border :data="spuList" style="width: 100%; margin:20px 0">
+        <el-table-column type="index" label="序号" width="80" align="center"></el-table-column>
+        <el-table-column prop="spuName" label="SPU名称"></el-table-column>
+        <el-table-column prop="description" label="SPU描述"></el-table-column>
+        <el-table-column prop="name" label="操作" width="260" align="center">
+          <TipButton type="success" icon="el-icon-plus" tipText="添加" />
+          <TipButton type="primary" icon="el-icon-edit" tipText="修改" />
+          <TipButton type="info" icon="el-icon-info" tipText="查看SPU列表" />
+          <TipButton type="warning" icon="el-icon-delete" tipText="删除" />
+        </el-table-column>
+      </el-table>
+
+      <!-- 分页器 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pager.currentPage"
+        :page-sizes="[2, 4, 6, 8, 10]"
+        :page-size="pager.pageSize"
+        layout="prev, pager, next, jumper, ->, sizes, total"
+        :total="pager.total"
+        style="text-align:center"
+      ></el-pagination>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
   name: 'SPU',
+  data() {
+    return {
+      loading: false, // loading状态
+      // 分页器
+      pager: {
+        currentPage: 1,
+        pageSize: 2,
+        total: 10,
+      },
+      category1Id: '',
+      category2Id: '',
+      category3Id: '',
+      isShowSpuList: true, // 是否展示Spu列表
+      spuList: [], // SPU数据列表
+    };
+  },
+  methods: {
+    // 获取并设置 spuList(发送请求)
+    async setSpuList() {
+      const {
+        pager: { currentPage, pageSize },
+        category3Id,
+      } = this;
+
+      // 展示加载状态
+      this.loading = true;
+      try {
+        // 获取 spuList (发送请求)
+        const {
+          data: { records, total },
+        } = await this.$API.spu.getSpuList(currentPage, pageSize, category3Id);
+
+        // 隐藏加载状态
+        this.loading = false;
+
+        this.spuList = records;
+        this.pager.total = total;
+      } catch (error) {
+        this.$message.error('亲,系统错误~~~');
+      }
+    },
+
+    // 分页器: 展示对应条数spu数据
+    handleSizeChange(pageSize) {
+      this.pager.pageSize = pageSize;
+      this.setSpuList();
+    },
+
+    // 分页器: 展示对应页码spu数据
+    handleCurrentChange(currentPage) {
+      this.pager.currentPage = currentPage;
+      this.setSpuList();
+    },
+  },
+  watch: {
+    category3Id(newVal) {
+      // 没有值则退出
+      if (!newVal) return;
+
+      // 有值则获取 spuList (发送请求)
+      this.setSpuList();
+    },
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.spu-wraper {
+  margin: 20px 0;
+}
+</style>
