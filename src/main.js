f948e802cbd30dd32fc20661b02bd2fa88aec3ed
luoxin <1813650185@qq.com>
2021-07-12 15:21:00 +0800

    首页静态搭建

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ import TipButton from '@/components/TipButton';
 
 import '@/icons'; // icon
 import '@/permission'; // permission control
+import '@/plugins/vcharts';
 
 /**
  * If you don't want to use mock-server
