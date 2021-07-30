f348faba29e7b4fb414f92cbd3eb5325b1ede414
dumpling <luoxin.email@qq.com>
2021-07-30 15:04:39 +0800

    完成登录

--- src/settings.js
+++ src/settings.js
@@ -1,15 +1,15 @@
 module.exports = {
-  title: "Vue Admin Template",
+    title: 'shangPinHui Admin',
 
-  /**
-   * @type {boolean} true | false
-   * @description Whether fix the header
-   */
-  fixedHeader: true,
+    /**
+     * @type {boolean} true | false
+     * @description Whether fix the header
+     */
+    fixedHeader: true,
 
-  /**
-   * @type {boolean} true | false
-   * @description Whether show the logo in sidebar
-   */
-  sidebarLogo: false
+    /**
+     * @type {boolean} true | false
+     * @description Whether show the logo in sidebar
+     */
+    sidebarLogo: false,
 };
